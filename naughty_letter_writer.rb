77afeab68e10c98afe738e5de35c11d3771f9aaa
Sanna Yamaguchi <sannabanana@Sannas-MacBook-Pro.local>
2018-11-29 16:26:54 -0500

    The Police - Every Breath You Take.

---
+++ naughty_letter_writer.rb
@@ -0,0 +1,32 @@
+require "erb"
+require "pry-nav"
+
+kids_data = File.read("data/kids-data.txt")
+naughty_letter = File.read("templates/naughty_letter_template.txt.erb")
+
+kids_data.each_line do |kid|
+  kid_data_array = kid.split
+
+  name = kid_data_array[0]
+  behavior = kid_data_array[1]
+  toys = kid_data_array[2..7]
+  infraction = kid.split("| ")
+  single_toy = ""
+
+  loop do
+    single_toy = toys[rand(6)]
+    break if single_toy != "Kaleidoscope"
+  end
+
+  toys.delete(single_toy)
+
+  next unless behavior == "naughty"
+
+  filename = "letters/naughty/" + name + ".txt"
+  letter_text = ERB.new(naughty_letter, nil, "-").result(binding)
+
+  puts "Writing #{filename}."
+  File.write(filename, letter_text)
+end
+
+puts "Done!"
